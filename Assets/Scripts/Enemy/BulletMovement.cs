d2597ccd50a87e4d0b2277fc59502198d5213783
kxmirand <kxmirand@calpoly.edu>
2026-02-12 17:08:47 -0800

    added enemy with shooting

---
+++ Assets/Scripts/Enemy/BulletMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    public float lifetime = 5f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
