06b466d7757ea9c0c39e4e150bc69a5753f3211e
Jacob Kelleran <jacob.kelleran@outlook.com>
2026-02-18 18:04:48 -0800

    Updated player prefab in final scene and fixed blink

--- Assets/Scripts/Modules/BlinkModule.cs
+++ Assets/Scripts/Modules/BlinkModule.cs
@@ -9,15 +9,15 @@ public class BlinkModule : Module
         Transform playerTransform = playerObject.transform;
 
         // Teleport the player in the direction they are looking, up to a maximum distance
-        Vector3 blinkDestination = playerTransform.position + direction.normalized * maxBlinkDistance;
+        Vector3 blinkDisplacement = direction.normalized * maxBlinkDistance;
 
         // Check for obstacles using a spherecast with a small radius to prevent teleporting inside walls or other objects
         float sphereRadius = 0.5f;
         if (Physics.SphereCast(playerTransform.position, sphereRadius, direction.normalized, out RaycastHit hitInfo, maxBlinkDistance))
         {
-            blinkDestination = playerTransform.position + direction.normalized * hitInfo.distance;
+            blinkDisplacement = direction.normalized * hitInfo.distance;
         }
 
-        playerTransform.position = blinkDestination;
+        playerTransform.GetComponent<CharacterController>().Move(blinkDisplacement);
     }
 }
