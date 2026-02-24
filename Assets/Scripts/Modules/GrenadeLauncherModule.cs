9b15e9e976f574af807115c4b9be752dd953c123
Jacob Kelleran <jacob.kelleran@outlook.com>
2026-02-24 14:35:42 -0800

    Added grenade launcher

---
+++ Assets/Scripts/Modules/GrenadeLauncherModule.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class GrenadeLauncherModule : Module
+{
+    public float shootForce = 20f;
+    public GameObject grenadePrefab;
+    public Transform shootPoint;
+
+    protected override void PerformAction(Vector3 direction)
+    {
+        GameObject grenade = Instantiate(grenadePrefab, shootPoint.position, Quaternion.identity);
+        Rigidbody rb = grenade.GetComponent<Rigidbody>();
+        rb?.AddForce(direction.normalized * shootForce, ForceMode.VelocityChange);
+    }
+}
