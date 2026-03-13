1df4e81a6766406b245a57e13b783b9188244082
Jacob Kelleran <jacob.kelleran@outlook.com>
2026-03-13 02:37:17 -0700

    Added barrier effects

--- Assets/Scripts/Modules/BarrierModule.cs
+++ Assets/Scripts/Modules/BarrierModule.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 public class BarrierModule : Module
 {
     [Header("Barrier Settings")]
+    public GameObject barrierCharge;
     public GameObject barrierPrefab;
 
     protected override void PerformAction(Vector3 direction)
     {
         GameObject projectile = Instantiate(barrierPrefab, transform.position, Quaternion.LookRotation(direction));
+        barrierCharge.SetActive(false);
+    }
+
+    protected override void OnCooldownComplete()
+    {
+        barrierCharge.SetActive(true);
     }
 }
