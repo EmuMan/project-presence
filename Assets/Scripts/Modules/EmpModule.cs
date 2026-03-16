be394b1106ae95bf93998bd6b34bce7ae741d928
Jacob Kelleran <jacob.kelleran@outlook.com>
2026-03-16 11:56:56 -0700

    Module bugfixes

--- Assets/Scripts/Modules/EmpModule.cs
+++ Assets/Scripts/Modules/EmpModule.cs
@@ -4,9 +4,22 @@ public class EmpModule : Module
 {
     [Header("EMP Settings")]
     public GameObject empPrefab;
+    public GameObject empChargeObject;
 
     protected override void PerformAction(Vector3 direction)
     {
         Instantiate(empPrefab, transform.position, Quaternion.LookRotation(direction));
+        if (empChargeObject != null)
+        {
+            empChargeObject.SetActive(false);
+        }
+    }
+
+    protected override void OnCooldownComplete()
+    {
+        if (empChargeObject != null)
+        {
+            empChargeObject.SetActive(true);
+        }
     }
 }
