cc99e78b2ae3b3c48107721391f4ca417dac72d0
Jacob Kelleran <jacob.kelleran@outlook.com>
2026-03-17 12:30:51 -0700

    Added effects to item pickups

--- Assets/Scripts/Modules/ModuleItem.cs
+++ Assets/Scripts/Modules/ModuleItem.cs
@@ -2,13 +2,44 @@ using UnityEngine;
 
 public class ModuleItem : MonoBehaviour
 {
+    [Header("Module Item Settings")]
     [SerializeField] private ModuleData moduleData;
-    [SerializeField] private GameObject pickupEffect;
     [SerializeField] private Transform instantiationPoint;
+
+    [Header("Effects")]
+    [SerializeField] private GameObject pickupEffectPrefab;
+    [SerializeField] private ParticleSystem trickleEffect;
+
+    [Header("Movement Settings")]
+    [SerializeField] private float rotationSpeed = 50f;
+    [SerializeField] private float bobbingAmplitude = 0.25f;
+    [SerializeField] private float bobbingFrequency = 0.5f;
+    [SerializeField] private float postPickupYOffset = -2.0f;
+    [SerializeField] private float postPickupMoveSpeed = -0.5f;
+
+    private float timeSinceStart = 0f;
+    private float moduleInitialY;
+    private float initialY;
+    private bool isPickedUp = false;
+    private float postPickupYDisplacement = 0f;
 
     void Start()
     {
         Instantiate(moduleData.instancePrefab, instantiationPoint);
+
+        moduleInitialY = instantiationPoint.position.y;
+    }
+
+    void Update()
+    {
+        if (!isPickedUp)
+        {
+            MoveModule();
+        }
+        else
+        {
+            MovePostPickup();
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -22,13 +53,54 @@ public class ModuleItem : MonoBehaviour
             }
 
             // Show pickup effect
-            if (pickupEffect != null)
+            if (pickupEffectPrefab != null)
             {
-                Instantiate(pickupEffect, transform.position, Quaternion.identity);
+                Instantiate(pickupEffectPrefab, transform.position, Quaternion.identity);
             }
 
-            // Destroy the module item after pickup
+            isPickedUp = true;
+            initialY = transform.position.y;
+            DisableTrickleEffect();
+
+            Destroy(instantiationPoint.gameObject);
+        }
+    }
+
+    private void MoveModule()
+    {
+        // Rotate the module item
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+
+        // Bob up and down
+        timeSinceStart += Time.deltaTime;
+        float yDisp = Mathf.Sin(timeSinceStart * bobbingFrequency * 2f * Mathf.PI) * bobbingAmplitude;
+        instantiationPoint.position = new Vector3(
+            instantiationPoint.position.x,
+            moduleInitialY + yDisp,
+            instantiationPoint.position.z
+        );
+    }
+
+    private void MovePostPickup()
+    {
+        postPickupYDisplacement += postPickupMoveSpeed * Time.deltaTime;
+        transform.position = new Vector3(
+            transform.position.x,
+            initialY + postPickupYDisplacement,
+            transform.position.z
+        );
+
+        if (postPickupYDisplacement <= postPickupYOffset)
+        {
             Destroy(gameObject);
+        }
+    }
+
+    private void DisableTrickleEffect()
+    {
+        if (trickleEffect != null)
+        {
+            trickleEffect.Stop();
         }
     }
 }
