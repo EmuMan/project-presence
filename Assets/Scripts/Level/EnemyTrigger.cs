3f124cafcb21413db4c3547dcd2008afe132f538
Jacob Kelleran <jacob.kelleran@outlook.com>
2026-03-19 17:42:43 -0700

    Adjusted soundtracks

--- Assets/Scripts/Level/EnemyTrigger.cs
+++ Assets/Scripts/Level/EnemyTrigger.cs
@@ -1,9 +1,14 @@
 using UnityEngine;
+using System.Collections;
 
 public class EnemyTrigger : MonoBehaviour
 {
     [Header("Enemies to activate")]
     public Enemy[] enemies;
+
+    [Header("Audio")]
+    [SerializeField] private AudioSource combatAudio;
+    [SerializeField] private float audioFadeOutDelay = 2f;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -14,6 +19,8 @@ public class EnemyTrigger : MonoBehaviour
             if (enemy != null)
                 enemy.Activate();
         }
+
+        PlayAudio();
     }
 
     private void OnTriggerExit(Collider other)
@@ -25,5 +32,37 @@ public class EnemyTrigger : MonoBehaviour
             if (enemy != null)
                 enemy.Deactivate();
         }
+
+        FadeOutAudio();
+    }
+
+    private void PlayAudio()
+    {
+        if (combatAudio != null && !combatAudio.isPlaying)
+        {
+            combatAudio.Play();
+        }
+    }
+
+    private void FadeOutAudio()
+    {
+        if (combatAudio != null && combatAudio.isPlaying)
+        {
+            StartCoroutine(FadeOutCoroutine());
+        }
+    }
+
+    private IEnumerator FadeOutCoroutine()
+    {
+        float startVolume = combatAudio.volume;
+
+        while (combatAudio.volume > 0)
+        {
+            combatAudio.volume -= startVolume * Time.deltaTime / audioFadeOutDelay;
+            yield return null;
+        }
+
+        combatAudio.Stop();
+        combatAudio.volume = startVolume;
     }
 }
